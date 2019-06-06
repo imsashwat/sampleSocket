ff7ac418a316b9a09c034e1dab6cc3637c77cf1a
shashwat <star1shashwat@gmail.com>
2019-06-07 00:53:41 +0530

    sample socket.io

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const path = require('path')
+const socketio = require('socket.io')
+const http = require('http')
+
+const app = express();
+const server = http.createServer(app)
+const io = socketio(server)
+
+let usersockets = {}
+
+app.use('/', express.static(path.join(__dirname, 'frontend')))
+
+io.on('connection', (socket) => {
+    console.log("New socket formed on " + socket.id)
+    //socket.emit('connected')
+
+    socket.on('login', (data) => {
+        //username is in date.user
+        usersockets[data.user] = socket.id
+        console.log(usersockets)
+    })
+
+    socket.on('send_msg', (data) => {
+        //if we use io.emit, everyone gets it
+        //if socket.broadcast.emit, only others get it
+        if(data.message.startsWith('@')) {
+            //data.message =
+            let receipient = data.message.split(':')[0].substr(1)
+            let recpSocket = usersockets[receipient]
+            io.to(recpSocket).emit('recv_msg',data)
+        }
+        else {
+        socket.broadcast.emit('recv_msg', data)
+        }
+    })
+})
+
+server.listen(2345, () => console.log('website open on http://localhost:2345'))
